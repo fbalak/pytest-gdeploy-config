394d08fc33687345f5ab7114e38565d7798a1e3e
Filip Balák <fbalak@redhat.com>
2017-07-24 08:19:37 -0400

    update tests, add test_gdeploy_config_fixture test

--- tests/test_gdeploy_config.py
+++ tests/test_gdeploy_config.py
@@ -1,28 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
-def test_bar_fixture(testdir):
-    """Make sure that pytest accepts our fixture."""
-
-    # create a temporary pytest test module
-    testdir.makepyfile("""
-        def test_sth(bar):
-            assert bar == "europython2015"
-    """)
-
-    # run pytest with the following cmd args
-    result = testdir.runpytest(
-        '--foo=europython2015',
-        '-v'
-    )
-
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        '*::test_sth PASSED',
-    ])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
+import os
+import textwrap
 
 
 def test_help_message(testdir):
@@ -32,5 +12,37 @@ def test_help_message(testdir):
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
         'gdeploy-config:',
-        '*--gdeploy-configuration-directory=CONFIG_DIR*',
+        '*--gdeploy-configuration-file=CONFIG_FILE*',
     ])
+
+def test_gdeploy_config_fixture(testdir):
+    """
+    Make sure that``gdeploy_config`` fixture is recognized and pytest
+    itself is not broken by running very simple configuration file which
+    has no side effects.
+    """
+    # create a temporary pytest test module
+    testdir.makepyfile(textwrap.dedent("""\
+        def test_foo(gdeploy_config):
+            assert 1 == 1
+        """))
+    # create minimal gdeploy configuration file
+    server = "localhost"
+    config = testdir.makefile(
+        ".conf",
+        "[hosts]",
+        server,
+        "",
+        "[shell]",
+        "action=execute",
+        "command=ping -c 1 {}".format(server))
+    # run pytest with the following cmd args
+    result = testdir.runpytest(
+        '--gdeploy-configuration-file={0}'.format(
+            os.path.join(config.dirname, config.basename)),
+        '-v'
+        )
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines(['*::test_foo PASSED'])
+    # make sure that that we get a '1' exit code for the testsuite
+    assert result.ret == 0
