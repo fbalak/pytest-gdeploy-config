b446c8231ec321d4cc56ada349290074b5456ef0
Filip Balák <fbalak@redhat.com>
2017-07-24 08:18:32 -0400

    implement initial gdeploy_config fixture

--- pytest_gdeploy_config.py
+++ pytest_gdeploy_config.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 
 
+from __future__ import print_function
+import subprocess
+
+import pytest
+
+
 def pytest_addoption(parser):
     group = parser.getgroup('gdeploy-config')
     group.addoption(
-        '--foo',
-        '--gdeploy-configuration-directory',
+        '--gdeploy-configuration-file',
         action='store',
-        dest='gdeploy_config_dir',
-        metavar='CONFIG_DIR',
-        help='Directory where gdeploy configuration files are stored.'
+        dest='gdeploy_config_file',
+        metavar="CONFIG_FILE",
+        help='gdeploy configuration file.',
     )
+
+
+@pytest.fixture
+def gdeploy_config(request):
+    """
+    Pytest fixture which runs given gdeploy configuration file.
+    """
+    gdeploy_command = [
+        'gdeploy',
+        '-c',
+        request.config.option.gdeploy_config_file,
+        ]
+    subprocess.check_call(gdeploy_command)
