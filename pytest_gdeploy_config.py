7f387310749494339a66f4c098c9567c8b52c307
Filip Balák <fbalak@redhat.com>
2017-07-25 10:26:51 -0400

    add setup and teardown markers, support more files

--- pytest_gdeploy_config.py
+++ pytest_gdeploy_config.py
@@ -28,9 +28,22 @@ def get_gdeploy_cmd(gdeploy_file):
     gdeploy_command = [
         'gdeploy',
         '-c',
-        request.config.option.gdeploy_config_file,
+        gdeploy_file
     ]
     return gdeploy_command
+
+
+def get_empty_marker_error(marker_type):
+    """
+    Generate error message for empty marker.
+    """
+    msg = (
+        "no configuration file is specified in "
+        "``@pytest.mark.gdeploy_config_{0}`` decorator "
+        "of this test case, please add at least one "
+        "configuration file file name as a parameter into the marker, eg. "
+        "``@pytest.mark.gdeploy_config_{0}('config.conf')``")
+    return msg.format(marker_type)
 
 
 @pytest.fixture
@@ -38,5 +51,41 @@ def gdeploy_config(request):
     """
     Pytest fixture which runs given gdeploy configuration file.
     """
-    gdeploy_command = get_gdeploy_cmd(request.config.option.gdeploy_config_file)
+    gdeploy_command = get_gdeploy_cmd(
+        request.config.option.gdeploy_config_file)
     subprocess.check_call(gdeploy_command)
+
+    setup_marker = request.node.get_marker('ansible_playbook_setup')
+    setup_files = []
+    teardown_marker = request.node.get_marker('ansible_playbook_teardown')
+    teardown_files = []
+
+    if setup_marker is None and teardown_marker is None:
+        msg = (
+            "no gdeploy configuration is specified for the test case, "
+            "please add a decorator like this one "
+            "``@pytest.mark.gdeploy_config_setup('config.conf')`` "
+            "or "
+            "``@pytest.mark.gdeploy_config_teardown('config.conf')`` "
+            "for gdeploy_config fixture to know which file to use")
+        raise Exception(msg)
+    if setup_marker is not None:
+        setup_files = setup_marker.args
+        if len(setup_marker.args) == 0:
+            raise Exception(get_empty_marker_error("setup"))
+    if teardown_marker is not None:
+        teardown_files = teardown_marker.args
+        if len(teardown_marker.args) == 0:
+            raise Exception(get_empty_marker_error("teardown"))
+
+    # setup
+    for config_file in setup_files:
+        subprocess.check_call(
+            get_gdeploy_cmd(
+                config_file))
+    yield
+    # teardown
+    for config_file in teardown_files:
+        subprocess.check_call(
+            get_gdeploy_cmd(
+                config_file))
