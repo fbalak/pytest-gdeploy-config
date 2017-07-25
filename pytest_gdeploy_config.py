cdc3d38170cd999aeab91a07591542ec10090020
Filip Balák <fbalak@redhat.com>
2017-07-25 09:50:46 -0400

    add docstring, add get_gdeploy_cmd() function

--- pytest_gdeploy_config.py
+++ pytest_gdeploy_config.py
@@ -8,6 +8,9 @@ import pytest
 
 
 def pytest_addoption(parser):
+    """
+    Define py.test command line options for this plugin.
+    """
     group = parser.getgroup('gdeploy-config')
     group.addoption(
         '--gdeploy-configuration-file',
@@ -18,14 +21,22 @@ def pytest_addoption(parser):
     )
 
 
-@pytest.fixture
-def gdeploy_config(request):
+def get_gdeploy_cmd(gdeploy_file):
     """
-    Pytest fixture which runs given gdeploy configuration file.
+    Return process args list for gdeploy run.
     """
     gdeploy_command = [
         'gdeploy',
         '-c',
         request.config.option.gdeploy_config_file,
-        ]
+    ]
+    return gdeploy_command
+
+
+@pytest.fixture
+def gdeploy_config(request):
+    """
+    Pytest fixture which runs given gdeploy configuration file.
+    """
+    gdeploy_command = get_gdeploy_cmd(request.config.option.gdeploy_config_file)
     subprocess.check_call(gdeploy_command)
