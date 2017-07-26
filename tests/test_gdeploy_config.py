2c62131e549010deda4398482cc61cd008bc9e13
Filip Balák <fbalak@redhat.com>
2017-07-26 07:48:12 -0400

    rename test_gdeploy_config_fixture to test_simple, update test_simple

--- tests/test_gdeploy_config.py
+++ tests/test_gdeploy_config.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 
-import os
 import textwrap
+
+
+import pytest
 
 
 def test_help_message(testdir):
@@ -13,10 +15,12 @@ def test_help_message(testdir):
     result.stdout.fnmatch_lines([
         'gdeploy-config:',
         '*--gdeploy-configuration-file=CONFIG_FILE*',
+        '*--configuration-directory=CONFIG_DIR*',
     ])
 
 
-def test_gdeploy_config_fixture(testdir, minimal_gdeploy_config):
+@pytest.mark.parametrize("marker_type", ["setup", "teardown"])
+def test_simple(testdir, minimal_gdeploy_config, marker_type):
     """
     Make sure that``gdeploy_config`` fixture is recognized and pytest
     itself is not broken by running very simple configuration file which
@@ -24,18 +28,20 @@ def test_gdeploy_config_fixture(testdir, minimal_gdeploy_config):
     """
     # create a temporary pytest test module
     testdir.makepyfile(textwrap.dedent("""\
+        import pytest
+        @pytest.mark.ansible_playbook_{0}('{1}')
         def test_foo(gdeploy_config):
             assert 1 == 1
-        """))
+        """.format(marker_type, minimal_gdeploy_config.basename)))
     # run pytest with the following cmd args
     result = testdir.runpytest(
         '--gdeploy-configuration-file={0}'.format(
-            os.path.join(
-                minimal_gdeploy_config.dirname,
-                minimal_gdeploy_config.basename)),
+            minimal_gdeploy_config.basename),
+        '--configuration-directory={0}'.format(
+            minimal_gdeploy_config.dirname),
         '-v'
         )
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines(['*::test_foo PASSED'])
-    # make sure that that we get a '1' exit code for the testsuite
+    # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
