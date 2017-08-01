806165e0e8abd65a52c43d1bd15815e778e1e120
Filip Balák <fbalak@redhat.com>
2017-08-01 07:45:59 -0400

    add testfile_config_generator fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,7 @@
+import os
+import random
+import string
+
 import pytest
 
 
@@ -19,3 +23,49 @@ def minimal_gdeploy_config(testdir):
         "action=execute",
         "command=ping -c 1 {}".format(server))
     return config
+
+
+@pytest.fixture
+def testfile_config_generator(testdir):
+    """
+    Return an object with ``get()`` method to generate a config file which
+    creates a test file along with expected path and content.
+
+    This is usefull when one needs one or more config_files with simple and
+    easy to check side effect.
+    """
+    class ConfigGenerator(object):
+        _id = 1
+
+        def get(self):
+            # create dummy temp file,
+            # so that we can check it's ``.dirname`` attribute later
+            dummy_file = testdir.makefile(".dummy", "")
+            # define file path for a test file which will be created
+            # by ansible-config_file run
+            test_file_path = os.path.join(
+                dummy_file.dirname,
+                "test_file.{0}".format(ConfigGenerator._id))
+            # generate random content of the test file
+            test_file_content = "".join(
+                random.choice(string.ascii_letters) for _ in range(15))
+            # create ansbile config_file file(which would create file on
+            # test_file_path with test_file_content in it)
+            config_file = testdir.makefile(
+                ".{0}.conf".format(ConfigGenerator._id),
+                "[hosts]",
+                "localhost",
+                "",
+                "[shell]",
+                "action=execute",
+                "command=touch {0}".format(test_file_path),
+                "",
+                "[update-file]",
+                "action=add",
+                "dest={0}".format(test_file_path),
+                "line={0}".format(test_file_content),
+                )
+            ConfigGenerator._id += 1
+            return config_file, test_file_path, test_file_content
+
+    return ConfigGenerator()
