3b2f028e7c964c464aa895f3b3ec66a3d787203d
Filip Balák <fbalak@redhat.com>
2017-08-01 09:17:47 -0400

    add test_teardown_checkfile

--- tests/test_gdeploy_config.py
+++ tests/test_gdeploy_config.py
@@ -173,3 +173,58 @@ def test_two_checkfile(
             assert content == exp_content + "\n"
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
+
+
+def test_teardown_checkfile(testdir, testfile_config_generator):
+    """
+    Make sure that ``gdeploy_config`` fixture actually executes
+    setup config_file during setup, and then teardown config_file during teardown.
+
+    This is done by making the check in the temporary pytest module
+    (see testdir.makepyfile call), which makes sure that:
+
+    * setup temporary file exists
+    * teardown temporary file doesn't exist
+    """
+    # setup config_file
+    config_file_1, filepath_1, content_1 = testfile_config_generator.get()
+    # teardown config_file
+    config_file_2, filepath_2, content_2 = testfile_config_generator.get()
+    # create a temporary pytest test module
+    testdir.makepyfile(textwrap.dedent("""\
+        import pytest
+
+        @pytest.mark.gdeploy_config_setup('{setup_config_file}')
+        @pytest.mark.gdeploy_config_teardown('{teardown_config_file}')
+        def test_proper_teardown(gdeploy_config):
+            with open('{setup_file_path}', 'r') as test_file_object:
+                content = test_file_object.read()
+                assert content == '{setup_exp_content}' + "\\n"
+            with pytest.raises(IOError):
+                open('{teardown_file_path}', 'r')
+        """.format(
+            setup_config_file=config_file_1.basename,
+            teardown_config_file=config_file_2.basename,
+            setup_file_path=filepath_1,
+            setup_exp_content=content_1,
+            teardown_file_path=filepath_2,
+            )))
+    # check assumption of this test case, if this fails, we need to rewrite
+    # this test case so that both config_file files ends in the same directory
+    assert config_file_1.dirname == config_file_2.dirname
+    # run pytest with the following cmd args
+    result = testdir.runpytest(
+        '--configuration-directory={0}'.format(config_file_1.dirname),
+        '-v',
+        )
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines(['*::test_proper_teardown PASSED'])
+    # check that test_file has been created
+    for file_path, exp_content in zip(
+            (filepath_1, filepath_2),
+            (content_1, content_2)):
+        with open(file_path, 'r') as test_file_object:
+            content = test_file_object.read()
+            assert content == exp_content + "\n"
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
